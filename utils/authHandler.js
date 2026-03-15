0c9342176c199f6cf4a4b3ea691fcf69e31ebc3b
Huybui-it <nhochuybui@gmail.com>
2026-03-15 14:10:03 +0700

    Add RS256 JWT, Change Password API, and key pairs

---
+++ utils/authHandler.js
@@ -0,0 +1,31 @@
+let userController = require('../controllers/users')
+let jwt = require('jsonwebtoken')
+module.exports = {
+    CheckLogin: async function (req, res, next) {
+        try {
+            let token = req.headers.authorization;
+            if (!token || !token.startsWith("Bearer")) {
+                res.status(403).send({ message: "ban chua dang nhap" })
+                return;
+            }
+            token = token.split(' ')[1]
+            let fs = require('fs');
+            let publicKey = fs.readFileSync('public.key', 'utf8');
+            let result = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+            if (result.exp * 1000 < Date.now()) {
+                res.status(403).send({ message: "ban chua dang nhap" })
+                return;
+            }
+            let getUser = await userController.GetUserById(result.id);
+            if (!getUser) {
+                res.status(403).send({ message: "ban chua dang nhap" })
+            } else {
+                req.user = getUser;
+                next();
+            }
+        } catch (error) {
+            res.status(403).send({ message: "ban chua dang nhap" })
+        }
+
+    }
+}
